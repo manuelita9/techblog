9ad3aba1f86bfdeb38bebc6414910e16c0c67a93
manuela medina <manu573@live.com>
2023-04-17 23:30:01 -0400

    update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,4 +22,36 @@ router.get('/', (req, res) => {
       // Get all the posts from the Post table, including the associated user
       const posts = await Post.findAll({ include: User });
   
-   
+      // Render the blog page with the posts as bootstrap cards
+      res.render('blogpage', { 
+        posts: posts.map(post => ({
+          id: post.id,
+          textContent: post.textContent,
+          date: post.date,
+          time: post.time,
+          userName: post.User?.name
+        }))
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
+
+router.post('/blog', withAuth, async (req, res) => {
+try {
+  // Create a new post with the current user's ID
+  await Post.create({
+    textContent: req.body.textContent,
+    userId: req.session.user_id
+  });
+
+  // Redirect back to the blog page
+  res.redirect('/blog');
+} catch (err) {
+  console.error(err);
+  res.status(500).json(err);
+}
+});
+
+module.exports = router;
