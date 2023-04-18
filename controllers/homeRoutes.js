ca117d5f2153e716c531b31dc4d4be6f1bdf7954
manuela medina <manu573@live.com>
2023-04-17 23:28:43 -0400

    update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { Post, User } = require('./../models');
 const withAuth = require('./../utils/auth');
+
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+  });
+  
+ 
