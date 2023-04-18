5a73aca45cc5b571d56256f261aae144a0567216
manuela medina <manu573@live.com>
2023-04-17 23:29:09 -0400

    code update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,4 +8,9 @@ router.get('/', (req, res) => {
     res.render('homepage');
   });
   
- 
+  router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
